3c0f360b78f1577e2d3eeeaad62216f7a7ccfd06
Mark Lane <marklane2001@hotmail.com>
2018-05-13 19:24:07 +0100

    Create homepage and add link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require rails-ujs
 //= require jquery
-//= require bootstrap-sprockets
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
 
